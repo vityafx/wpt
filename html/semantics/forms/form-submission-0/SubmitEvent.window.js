ad918cec5107f4d863d9d69abbab40b4bf33ae73
Jihwan Marc Kim <bluewhale.marc@gmail.com>
2019-12-22 06:13:47 -0800

    Implement SubmitEvent.
    Add SubmitEvent interface.
    Add SubmitEvent constructor and SubmitEventInit dictionary.
    
    Dispatch 'submitter' and tests will be added later.
    
    Bug: 1014418
    Change-Id: I8fb4421e46dfddbd4897aac000f9aa4884c3ca7f

--- html/semantics/forms/form-submission-0/SubmitEvent.window.js
+++ html/semantics/forms/form-submission-0/SubmitEvent.window.js
@@ -4,12 +4,6 @@ test(() => {
   let button = document.createElement('button');
   let typeError = new TypeError();
   assert_throws(typeError, () => { new SubmitEvent() }, '0 arguments');
-  assert_throws(typeError, () => { new SubmitEvent('bar', button) }, '1 invalid arguments');
-  assert_throws(typeError, () => { new SubmitEvent(button, button) }, '2 invalid arguments');
-  assert_throws(typeError, () => { new SubmitEvent('foo', null) }, 'Null dictionary');
-  assert_throws(typeError, () => { new SubmitEvent('foo', undefined) }, 'Undefined dictionary');
-  assert_throws(typeError, () => { new SubmitEvent('foo', { submitter: null }) }, 'Null submitter');
-  assert_throws(typeError, () => { new SubmitEvent('foo', { submitter: undefined }) }, 'Undefined submitter');
   assert_throws(typeError, () => { new SubmitEvent('foo', { submitter: 'bar' }) }, 'Wrong type of submitter');
 }, 'Failing SubmitEvent constructor');
 
@@ -21,9 +15,18 @@ test(() => {
 }, 'Successful SubmitEvent constructor');
 
 test(() => {
-  let event = new SubmitEvent('bar', { submitter: null});
-  assert_equals(event.submitter, null);
+  let event1 = new SubmitEvent('bar', { submitter: null });
+  assert_equals(event1.submitter, null);
+  let event2 = new SubmitEvent('bar', null);
+  assert_equals(event2.submitter, null);
 }, 'Successful SubmitEvent constructor; null submitter');
+
+test (() => {
+  let event1 = new SubmitEvent('baz', { submitter: undefined });
+  assert_equals(event1.submitter, null);
+  let event2 = new SubmitEvent('baz', undefined);
+  assert_equals(event2.submitter, null);
+}, 'Successful SubmitEvent constructor; undefined dictionary')
 
 test(() => {
   let event = new SubmitEvent('baz', {});
@@ -31,6 +34,9 @@ test(() => {
 }, 'Successful SubmitEvent constructor; empty dictionary');
 
 test(() => {
-  let event = new SubmitEvent('baz');
-  assert_equals(event.submitter, null);
+  let button = document.createElement('button');
+  let event1 = new SubmitEvent('baz');
+  assert_equals(event1.submitter, null);
+  let event2 = new SubmitEvent("bax", button);
+  assert_equals(event2.submitter, null);
 }, 'Successful SubmitEvent constructor; missing dictionary');
